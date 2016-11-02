addd953760d8a60c2701f5a963169c74b4f4fdb9
Nick Popov <nckcol@gmail.com>
2016-11-02 20:06:46 +0200

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import webpack from "webpack";
+
+export default {
+  context: __dirname + "/src",
+  entry: {
+    app: "./app.js",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js",
+  },
+};
